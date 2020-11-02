7510792b08e174cb5b88ba0a02e665c844fe2772
AnshuJalan <anshujalan206@gmail.com>
2020-11-02 18:07:41 +0530

    streaming actions

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,26 +1,27 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+
+import { createStream } from "../../actions";
+
+function renderInput({ input, label, meta }) {
+  return (
+    <div className="form-group">
+      <label className="font-weight-bold mb-0">{label}</label>
+      <input
+        className={`form-control ${meta.error && meta.touched && "is-invalid"}`}
+        value={input.value}
+        onChange={input.onChange}
+        autoComplete="off"
+      />
+      <div className="invalid-feedback">{meta.touched && meta.error}</div>
+    </div>
+  );
+}
 
 const StreamCreate = (props) => {
-  const renderInput = ({ input, label, meta }) => {
-    return (
-      <div className="form-group">
-        <label className="font-weight-bold mb-0">{label}</label>
-        <input
-          className={`form-control ${
-            meta.error && meta.touched && "is-invalid"
-          }`}
-          value={input.value}
-          onChange={input.onChange}
-          autoComplete="off"
-        />
-        <div className="invalid-feedback">{meta.touched && meta.error}</div>
-      </div>
-    );
-  };
-
   const onSubmit = (formValues) => {
-    console.log(formValues);
+    props.createStream(formValues);
   };
 
   return (
@@ -49,7 +50,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
